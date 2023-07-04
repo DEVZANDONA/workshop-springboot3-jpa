8687def4696ab214d44ef2a7fc573b2ac1091c56
Pedro Zandoná <pedruco.zandona@gmail.com>
2023-07-04 19:45:39 -0300

    Relacionando Product com Category com JoinTable

--- src/main/java/com/educandoweb/Course/entities/Product.java
+++ src/main/java/com/educandoweb/Course/entities/Product.java
@@ -1,6 +1,7 @@
 package com.educandoweb.Course.entities;
 
 import java.io.Serializable;
+
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -9,30 +10,34 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
+
 
 @Entity
-@Table(name="TB_PRODUCT")
-public class Product implements Serializable{
+@Table(name = "TB_PRODUCT")
+public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String name;
 	private String decription;
 	private Double price;
 	private String imgUrl;
-	
-	//Associações
-	@Transient
+
+	// Associações muitos para muitos
+	@ManyToMany
+	@JoinTable(name = "TB_PRODUCT_CATEGORY", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
-	
+
 	public Product() {
-		
+
 	}
 
 	public Product(Long id, String name, String decription, Double price, String imgUrl) {
@@ -109,10 +114,5 @@ public class Product implements Serializable{
 		return "Product [id=" + id + ", name=" + name + ", decription=" + decription + ", price=" + price + ", imgUrl="
 				+ imgUrl + ", categories=" + categories + "]";
 	}
-
-
-	
-	
-	
 
 }
